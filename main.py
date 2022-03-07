df555a7cdd3f9ae4658f8fbbe24475e17f9bbdd1
Chand Goraya <64827677+MoonGoraya@users.noreply.github.com>
2022-03-07 20:05:01 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+print("wellcome to anonymous hacker")
+name= input("what is your name ")
+greating="Hello"
+print(greating,name)
+print("wellcome in all world best hacking agency")
+print("Contact Me Our Whatsapp Number 0000000  ")
